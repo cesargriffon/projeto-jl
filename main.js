0a1a8316321009f6e7fb8336771d00836cf93051
Cesar N. Silva <77632665+cesargriffon@users.noreply.github.com>
2021-12-26 22:23:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+const nav = document.querySelector("#header nav")
+const toggle = document.querySelectorAll("nav .toggle")
+
+for(const element of toggle) {
+    element.addEventListener('click', function(){
+        nav.classList.toggle('show')
+    })
+}
+
+const links = document.querySelectorAll("nav ul li a")
+for (const link of links){
+    link.addEventListener('click', function(){
+        nav.classList.remove('show')
+    })
+}
+
+/* mudar header da pagina quando der scroll */
+
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY >= navHeight){
+        header.classList.add("scroll")
+    }else
+        header.classList.remove("scroll")
+})
+
+ /* Scrollreveal: mostrar elementos quando der scroll na pagina*/
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials, #testimonials .testimonial,
+   #contact .text, #contact .links
+    `,
+     { interval: 100 })
